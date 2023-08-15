fd87b70e3c158480cc87929d5aea39391b9f2903
Sid <jsiddhesh40@gmail.com>
2023-08-16 02:35:37 +0530

    second commit

---
+++ location.js
@@ -0,0 +1,139 @@
+
+
+function mumbai(){
+    Mumbaimap.style.display="grid";
+    Chennaimap.style.display="none";
+    NaviMumbai.style.display="none";
+    Punjab.style.display="none";
+    Haryana.style.display="none";
+    Karanataka.style.display="none";
+    Haryana2.style.display="none";
+    Nashik.style.display="none";
+    NewDelhi.style.display="none";
+
+
+
+
+}
+
+
+function chennai(){
+    Chennaimap.style.display="grid";
+    Mumbaimap.style.display="none";
+    NaviMumbai.style.display="none";
+    Punjab.style.display="none";
+    Haryana.style.display="none";
+    Haryana2.style.display="none";
+    NewDelhi.style.display="none";
+
+    Karanataka.style.display="none";
+    Nashik.style.display="none";
+
+
+
+}
+function navimumbai(){
+    NaviMumbai.style.display="grid";
+    Mumbaimap.style.display="none";
+    Chennaimap.style.display="none";
+    Punjab.style.display="none";
+    Haryana.style.display="none";
+    Karanataka.style.display="none";
+    Haryana2.style.display="none";
+    NewDelhi.style.display="none";
+
+    Nashik.style.display="none";
+
+
+
+}
+
+function punjab(){
+    Punjab.style.display="grid";
+    Mumbaimap.style.display="none";
+    Chennaimap.style.display="none";
+    NaviMumbai.style.display="none";
+    Haryana.style.display="none";
+    Karanataka.style.display="none";
+    Haryana2.style.display="none";
+    NewDelhi.style.display="none";
+
+    Nashik.style.display="none";
+
+
+}
+
+
+function haryana(){
+    Haryana.style.display="grid";
+    Mumbaimap.style.display="none";
+    Chennaimap.style.display="none";
+    NaviMumbai.style.display="none";
+    Punjab.style.display="none";
+    Karanataka.style.display="none";
+    Haryana2.style.display="none";
+    Nashik.style.display="none";
+    NewDelhi.style.display="none";
+
+
+}
+
+function karanataka(){
+    Karanataka.style.display="grid";
+    Haryana.style.display="none";
+    Mumbaimap.style.display="none";
+    Chennaimap.style.display="none";
+    NaviMumbai.style.display="none";
+    Punjab.style.display="none";
+    Haryana2.style.display="none";
+    Nashik.style.display="none";
+    NewDelhi.style.display="none";
+
+
+
+}
+
+function haryana2(){
+    Haryana2.style.display="grid";
+    Karanataka.style.display="none";
+    Haryana.style.display="none";
+    Mumbaimap.style.display="none";
+    Chennaimap.style.display="none";
+    NaviMumbai.style.display="none";
+    Punjab.style.display="none";
+    Karanataka.style.display="none";
+    Nashik.style.display="none";
+    NewDelhi.style.display="none";
+
+}
+
+
+function nashik(){
+    Nashik.style.display="grid";
+    Haryana2.style.display="none";
+    Karanataka.style.display="none";
+    Haryana.style.display="none";
+    Mumbaimap.style.display="none";
+    Chennaimap.style.display="none";
+    NaviMumbai.style.display="none";
+    Punjab.style.display="none";
+    Karanataka.style.display="none";
+    Haryana2.style.display="none";
+    NewDelhi.style.display="none";
+
+}
+
+function newdelhi(){
+    NewDelhi.style.display="grid";
+    Nashik.style.display="none";
+    Haryana2.style.display="none";
+    Karanataka.style.display="none";
+    Haryana.style.display="none";
+    Mumbaimap.style.display="none";
+    Chennaimap.style.display="none";
+    NaviMumbai.style.display="none";
+    Punjab.style.display="none";
+    Karanataka.style.display="none";
+    Haryana2.style.display="none";
+    Nashik.style.display="none";
+}
